5d4c5cf10d8d2a16e1ca396cc8ee9f994a959804
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-13 13:50:05 -0500

    Update fedora-workstation.sh

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -1,26 +1,17 @@
-#!/bin/bash
-
-# Configure dnf (In order: automatically select fastest mirror, parallel downloads, and disable telemetry)
-# fastestmirror=1
 printf "%s" "
 max_parallel_downloads=10
 countme=false
 " | sudo tee -a /etc/dnf/dnf.conf
 
-# Setup RPMFusion
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 sudo dnf groupupdate core -y
 
 
-# echo 'Make sure your system has been fully-updated by running "sudo dnf upgrade -y" and reboot it once.'
 sudo dnf upgrade -y
 
-#Setting umask to 077
-# No one except wheel user and root get read/write files
 umask 077
 sudo sed -i 's/umask 022/umask 077/g' /etc/bashrc
 
-# Debloat
 debloat () {
 	log "debloat"
 	local -a debloating_stuff
@@ -114,30 +105,26 @@ sudo dnf -y rm ${my_debloating_stuff[*]}
 }
 my_debloat
 
-# Run Updates
 sudo dnf autoremove -y
 sudo fwupdmgr get-devices
 sudo fwupdmgr refresh --force
 sudo fwupdmgr get-updates -y
 sudo fwupdmgr update -y
 
-# Configure GNOME
 gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
 #gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 
-# Setup Flathub beta and third party packages
 sudo fedora-third-party enable
 sudo fedora-third-party refresh
 flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 
-# Debloat
-trafotin_flathub () {
-	log "trafotin_flathub"
-	local -a trafotin_flathub_install
-	trafotin_flathub_install=(
+flathub () {
+	log "flathub"
+	local -a flathub_install
+	flathub_install=(
 	"app.drey.Warp"
 	"com.github.finefindus.eyedropper"
 	"com.github.tchx84.Flatseal"
@@ -169,66 +156,48 @@ trafotin_flathub () {
 	"org.signal.Signal"
 	"re.sonny.Junction"
 )
-flatpak install -y flathub ${trafotin_flathub_install[*]}
+flatpak install -y flathub ${flathub_install[*]}
 }
-trafotin_flathub
+flathub
 
-# Installing some gaming and emulation related things
-flatpak install flathub org.duckstation.DuckStation net.pcsx2.PCSX2 org.ppsspp.PPSSPP org.ryujinx.Ryujinx info.cemu.Cemu app.xemu.xemu com.obsproject.Studio com.heroicgameslauncher.hgl
 
-# Setup Edge RPMs
-# https://brave.com/linux/#fedora-rockyrhel
+flatpak install flathub org.duckstation.DuckStation net.pcsx2.PCSX2 org.ppsspp.PPSSPP org.ryujinx.Ryujinx info.cemu.Cemu app.xemu.xemu
+
+
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 
 
-# Install RPMs
 sudo dnf install -y dnf-plugins-core steam-devices neovim sqlite3 zsh-autosuggestions zsh-syntax-highlighting setroubleshoot newsboat ffmpeg compat-ffmpeg4 akmod-v4l2loopback yt-dlp @virtualization guestfs-tools podman simple-scan google-chrome microsoft-edge-stable steam gparted --best --allowerasing
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
 
-# Initialize virtualization
 sudo sed -i 's/#unix_sock_group = "libvirt"/unix_sock_group = "libvirt"/g' /etc/libvirt/libvirtd.conf
 sudo sed -i 's/#unix_sock_rw_perms = "0770"/unix_sock_rw_perms = "0770"/g' /etc/libvirt/libvirtd.conf
 sudo systemctl enable libvirtd
 sudo usermod -aG libvirt "$(whoami)"
 
-# Cockpit is still missing some core functionality, but will switch when it is added.
-#sudo systemctl enable cockpit.socket --now
-
-# Harden the Kernel with Kicksecure's patches
-# Disables CD ROMs, FireWire, default writes, various kernel flags.
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/modprobe.d/30_security-misc.conf -o /etc/modprobe.d/30_security-misc.conf
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc.conf -o /etc/sysctl.d/30_security-misc.conf
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/a9886a3119f9b662b15fc26d28a7fedf316b72c4/usr/lib/sysctl.d/30_silent-kernel-printk.conf -o /etc/sysctl.d/30_silent-kernel-printk.conf
 
-# Enable Kicksecure CPU mitigations
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/default/grub.d/40_cpu_mitigations.cfg -o /etc/grub.d/40_cpu_mitigations.cfg
-# Kicksecure's CPU distrust script
+
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/default/grub.d/40_distrust_cpu.cfg -o /etc/grub.d/40_distrust_cpu.cfg
-# Enable Kicksecure's IOMMU patch (limits DMA)
-# New url (2024-04-18): https://github.com/Kicksecure/security-misc/raw/a9886a3119f9b662b15fc26d28a7fedf316b72c4/etc/default/grub.d/40_enable_iommu.cfg
+
 sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/default/grub.d/40_enable_iommu.cfg -o /etc/grub.d/40_enable_iommu.cfg
 
-# Divested's brace patches
-# Sandbox the brace systemd permissions
-# If you have VPN issues: https://old.reddit.com/r/DivestOS/comments/12b4fk4/comment/jex4qt2/
+
 sudo mkdir -p /etc/systemd/system/NetworkManager.service.d
 sudo curl https://gitlab.com/divested/brace/-/raw/master/brace/usr/lib/systemd/system/NetworkManager.service.d/99-brace.conf -o /etc/systemd/system/NetworkManager.service.d/99-brace.conf
 sudo mkdir -p /etc/systemd/system/irqbalance.service.d
 sudo curl https://gitlab.com/divested/brace/-/raw/master/brace/usr/lib/systemd/system/irqbalance.service.d/99-brace.conf -o /etc/systemd/system/irqbalance.service.d/99-brace.conf
 
-
-# caps the system usage of sshd
 sudo mkdir -p /etc/systemd/system/sshd.service.d
 sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/systemd/system/sshd.service.d/local.conf -o /etc/systemd/system/sshd.service.d/local.conf
-# echo "GSSAPIAuthentication no" | sudo tee /etc/ssh/ssh_config.d/10-custom.conf
-# echo "VerifyHostKeyDNS yes" | sudo tee -a /etc/ssh/ssh_config.d/10-custom.conf
 
-# NTS instead of NTP
-# NTS is a more secured version of NTP
 sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf -o /etc/chrony.conf
 
-# Remove Firewalld's Default Rules
+
 sudo firewall-cmd --permanent --remove-port=1025-65535/udp
 sudo firewall-cmd --permanent --remove-port=1025-65535/tcp
 sudo firewall-cmd --permanent --remove-service=mdns
@@ -236,7 +205,7 @@ sudo firewall-cmd --permanent --remove-service=ssh
 sudo firewall-cmd --permanent --remove-service=samba-client
 sudo firewall-cmd --reload
 
-#Randomize MAC address and disable static hostname. This could be used to track general network activity.
+
 sudo bash -c 'cat > /etc/NetworkManager/conf.d/00-macrandomize.conf' <<-'EOF'
 [main]
 hostname-mode=none
@@ -252,9 +221,7 @@ EOF
 sudo systemctl restart NetworkManager
 sudo hostnamectl hostname "localhost"
 
-# Make the Home folder private
-# Privatizing the home folder creates problems with virt-manager
-# accessing ISOs from your home directory. Store images in /var/lib/libvirt/images
+
 chmod 700 /home/"$(whoami)"
 # is reset using:
 #chmod 755 /home/"$(whoami)"
