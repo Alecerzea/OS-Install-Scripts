39752ddd286c0aedb247c58efa726d732bcb8df1
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-13 13:15:54 -0500

    Update Script.bat

--- Script.bat
+++ Script.bat
@@ -5,7 +5,7 @@ powercfg.exe —h off
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
 cleanmgr.exe /AUTOCLEAN 
-winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++  VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher HeroicGamesLauncher.HeroicGamesLauncher Ubisoft.Connect ElectronicArts.EADesktop PPSSPPTeam.PPSSPP M2Team.NanaZip Google.Chrome.EXE Mozilla.Firefox 9WZDNCRD29V9
+winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++  VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher HeroicGamesLauncher.HeroicGamesLauncher Ubisoft.Connect ElectronicArts.EADesktop GOG.Galaxy PPSSPPTeam.PPSSPP M2Team.NanaZip Google.Chrome.EXE Mozilla.Firefox 9WZDNCRD29V9
 & ([scriptblock]::Create((irm "https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1")))  
 chkdsk /f
 chkdsk /r
