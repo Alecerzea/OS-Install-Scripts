27deecf83620be9dd53cd12acac6212aa374ed8b
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-13 11:34:27 -0500

    Add files via upload

---
+++ Script for VMs.bat
@@ -0,0 +1,11 @@
+winget upgrade —all —include—unknown 
+DISM.exe /Online /Set-ReservedStorageState /State:Disabled
+DISM.exe /online /cleanup-image /startcomponentcleanup
+powercfg.exe —h off
+cleanmgr /sageset:l 
+cleanmgr /sagerun:l 
+cleanmgr.exe /AUTOCLEAN 
+winget install Brave.Brave MullvadVPN.MullvadBrowser Proton.ProtonVPN Proton.ProtonPass Microsoft.VisualStudioCode Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit GitHub.GitHubDesktop Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++ VideoLAN.VLC Git.Git M2Team.NanaZip 9WZDNCRD29V9
+& ([scriptblock]::Create((irm "https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1")))  
+chkdsk /f
+chkdsk /r
