4e4309af6e224891a7f5a7fa10c8c490e4f336a5
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-13 21:47:17 -0500

    Update Windows VMs.bat

--- Windows VMs.bat
+++ Windows VMs.bat
@@ -1,12 +1,10 @@
-winget upgrade —all —include—unknown 
-DISM.exe /Online /Set-ReservedStorageState /State:Disabled
-DISM.exe /online /cleanup-image /startcomponentcleanup
 powercfg.exe —h off
+winget upgrade —all —include—unknown 
+DISM.exe /online /cleanup-image /startcomponentcleanup
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
 cleanmgr.exe /AUTOCLEAN 
+DISM.exe /Online /Set-ReservedStorageState /State:Disabled
 winget install Brave.Brave MullvadVPN.MullvadBrowser MullvadVPN.MullvadVPN Microsoft.VisualStudioCode Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit GitHub.GitHubDesktop Easeware.DriverEasy Notepad++.Notepad++ VideoLAN.VLC Git.Git M2Team.NanaZip 9WZDNCRD29V9
-wsl --install
-wsl --set-default-version 2
 chkdsk /f
 chkdsk /r
