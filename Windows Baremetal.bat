97984937085466c57a6b3c086b9364a05eefa643
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-13 21:47:43 -0500

    Update Windows Baremetal.bat

--- Windows Baremetal.bat
+++ Windows Baremetal.bat
@@ -1,10 +1,10 @@
 powercfg.exe —h off
 winget upgrade —all —include—unknown 
 DISM.exe /online /cleanup-image /startcomponentcleanup
-DISM.exe /Online /Set-ReservedStorageState /State:Disabled
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
 cleanmgr.exe /AUTOCLEAN 
+DISM.exe /Online /Set-ReservedStorageState /State:Disabled
 winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++  VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher HeroicGamesLauncher.HeroicGamesLauncher Ubisoft.Connect ElectronicArts.EADesktop GOG.Galaxy PPSSPPTeam.PPSSPP M2Team.NanaZip Google.Chrome.EXE Mozilla.Firefox 9WZDNCRD29V9
 wsl --install
 wsl --set-default-version 2
