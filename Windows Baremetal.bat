3536667b7f5c8bf92991c1fbaafeddde48e6c968
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-17 10:18:59 -0500

    Update Windows Baremetal.bat

--- Windows Baremetal.bat
+++ Windows Baremetal.bat
@@ -5,7 +5,7 @@ cleanmgr /sageset:l
 cleanmgr /sagerun:l 
 cleanmgr.exe /AUTOCLEAN 
 DISM.exe /Online /Set-ReservedStorageState /State:Disabled
-winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++  VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher HeroicGamesLauncher.HeroicGamesLauncher Ubisoft.Connect ElectronicArts.EADesktop GOG.Galaxy PPSSPPTeam.PPSSPP M2Team.NanaZip Google.Chrome.EXE Mozilla.Firefox 9WZDNCRD29V9
+winget install Brave.Brave MullvadVPN.MullvadBrowser Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++  VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher HeroicGamesLauncher.HeroicGamesLauncher Ubisoft.Connect ElectronicArts.EADesktop GOG.Galaxy PPSSPPTeam.PPSSPP M2Team.NanaZip Google.Chrome.EXE Mozilla.Firefox 9WZDNCRD29V9
 wsl --install
 wsl --set-default-version 2
 chkdsk /f
