c19cb0e69a9a40913531fba4bb362900018e3d8c
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-17 23:02:59 -0500

    Update fedora-kde.sh

--- fedora-kde.sh
+++ fedora-kde.sh
@@ -105,7 +105,3 @@ chmod 700 /home/"$(whoami)"
 # In Wine, Easy AntiCheat requires Wine to use ptrace as a standard user.
 # Kicksecure limits this to root, but the workaround in this file is not comprehensive.
 sudo sed -i 's,kernel.yama.ptrace_scope=2,#kernel.yama.ptrace_scope=2,g' /etc/sysctl.d/30_security-misc.conf
-sudo dnf install kmodtool akmods mokutil openssl
-sudo kmodgenca -a
-sudo mokutil --import /etc/pki/akmods/certs/public_key.der
-systemctl reboot
