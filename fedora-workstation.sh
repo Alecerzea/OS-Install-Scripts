08a41f4dc264a98eb915785440514cf170cb6dc2
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-06-28 21:14:43 -0500

    Update fedora-workstation.sh so I add rpm inside dnf.conf

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -1,10 +1,8 @@
 printf "%s" "
 max_parallel_downloads=10
 countme=false
+deltarpm=true
 " | sudo tee -a /etc/dnf/dnf.conf
-
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
-sudo dnf groupupdate core -y
 
 
 sudo dnf upgrade -y
