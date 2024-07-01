770c863453f758e7a889bea33da1792e4f4bd9f6
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-01 00:47:46 -0500

    Update Windows-KVM.bat

--- Windows-KVM.bat
+++ Windows-KVM.bat
@@ -7,6 +7,6 @@ cleanmgr /sageset:l
 cleanmgr /sagerun:l 
 cleanmgr.exe /AUTOCLEAN 
 DISM.exe /Online /Set-ReservedStorageState /State:Disabled
-winget install Brave.Brave MullvadVPN.MullvadBrowser MullvadVPN.MullvadVPN Microsoft.VisualStudioCode Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit GitHub.GitHubDesktop Easeware.DriverEasy Notepad++.Notepad++ VideoLAN.VLC Git.Git RARLab.WinRAR
+winget install Brave.Brave MullvadVPN.MullvadBrowser Microsoft.VisualStudioCode Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit GitHub.GitHubDesktop Easeware.DriverEasy Notepad++.Notepad++ VideoLAN.VLC Git.Git RARLab.WinRAR
 chkdsk /f
 chkdsk /r
