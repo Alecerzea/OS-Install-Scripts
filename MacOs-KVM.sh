8ca3b0ff7ac724246179dec98d2427f221f29df1
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-07 07:09:02 -0500

    taken out some commands I dont need anymore since I stopped using anydesk in my daily basis

--- MacOs-KVM.sh
+++ MacOs-KVM.sh
@@ -12,8 +12,6 @@ nvram boot-args
 
 sudo nvram boot-args="serverperfmode=1 $(nvram boot-args 2>/dev/null | cut -f 2-)"
 
-sudo defaults write /Library/Preferences/com.apple.loginwindow DesktopPicture ""
-
 defaults write com.apple.Accessibility DifferentiateWithoutColor -int 1
 defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
 defaults write com.apple.universalaccess reduceMotion -int 1
@@ -33,13 +31,6 @@ defaults write com.apple.universalaccessAuthWarning com.apple.Terminal -bool tru
 defaults write com.apple.loginwindow DisableScreenLock -bool true
 
 defaults write com.apple.loginwindow TALLogoutSavesState -bool false
-
-
-defaults write com.apple.universalaccessAuthWarning "/Applications/AnyDesk.app" -bool true
-defaults write com.apple.universalaccessAuthWarning "/Applications/AnyDesk.app/Contents/MacOS/AnyDesk" -bool true
-defaults write com.apple.universalaccessAuthWarning "3::/Applications" -bool true
-defaults write com.apple.universalaccessAuthWarning "3::/Applications/AnyDesk.app" -bool true
-defaults write com.apple.universalaccessAuthWarning "com.philandro.anydesk" -bool true
 
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart \
     -activate \
