97337b22ba239e69aa7dc82918e6e7a04b92eac1
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-07 15:30:37 -0500

    adding Firefox to my Silverblue-to-Bazzite.sh

--- Silverblue-to-Bazzite.sh
+++ Silverblue-to-Bazzite.sh
@@ -1,5 +1,7 @@
 #only do these after installing a separate browser from the default firefox on Silverblue, that version is part of the Silverblue image and the version in Bazzite is a separate flatpak
 
+flatpak install org.mozilla.firefox
+
 sudo rpm-ostree reset
 
 rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/bazzite-deck-gnome:stable
