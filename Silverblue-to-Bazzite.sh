2c0d74fcb26708a5a0466a70da467793b1f1b8f0
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-07 06:59:13 -0500

    Explaining some firefox mess with silverblue-bazzite

--- Silverblue-to-Bazzite.sh
+++ Silverblue-to-Bazzite.sh
@@ -1,3 +1,5 @@
+#only do these after installing a separate browser from the default firefox on Silverblue, that version is part of the Silverblue image and the version in Bazzite is a separate flatpak
+
 sudo rpm-ostree reset
 
 rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/bazzite-deck-gnome:stable
