07936b1e7dea26a8bfe00840ab26adcfce910b67
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-07 06:55:45 -0500

    updating ytdlp names in my windows .bat

--- Windows-Baremetal.bat
+++ Windows-Baremetal.bat
@@ -5,7 +5,7 @@ cleanmgr /sageset:l
 cleanmgr /sagerun:l 
 cleanmgr.exe /AUTOCLEAN
 DISM.exe /Online /Set-ReservedStorageState /State:Disabled
-winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++ VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher GOG.Galaxy Amazon.Games Ubisoft.Connect ElectronicArts.EADesktop PPSSPPTeam.PPSSPP RARLab.WinRAR Google.Chrome.EXE Mozilla.Firefox Nvidia.GeForceNow PlayStation.PSRemotePlay PlayStation.PSPlus PlayStation.DualSenseFWUpdater yt-dlp
+winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++ VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher GOG.Galaxy Amazon.Games Ubisoft.Connect ElectronicArts.EADesktop PPSSPPTeam.PPSSPP RARLab.WinRAR Google.Chrome.EXE Mozilla.Firefox Nvidia.GeForceNow PlayStation.PSRemotePlay PlayStation.PSPlus PlayStation.DualSenseFWUpdater yt-dlp.yt-dlp
 wsl --install
 wsl --set-default-version 2
 ipconfig /flushdns #delete my incognito, you haven't seen anything 
