d5c31c45485f19b81d12f14c2a8221e4d4687e76
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-08 01:31:30 -0500

    taken out the firefox install as it would be redundant since I already have it in my silverblue.sh

--- Silverblue-to-Bazzite.sh
+++ Silverblue-to-Bazzite.sh
@@ -1,7 +1,5 @@
 #only do these after installing a separate browser from the default firefox on Silverblue, that version is part of the Silverblue image and the version in Bazzite is a separate flatpak
 
-flatpak install flathub org.mozilla.firefox
-
 sudo rpm-ostree reset
 
 rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/bazzite-deck-gnome:stable
