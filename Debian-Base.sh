2cccfcf63086c5bc6c69640002bf805109a65acf
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-08 01:03:38 -0500

    Adding yt-dlp repo on Debian

--- Debian-Base.sh
+++ Debian-Base.sh
@@ -42,6 +42,9 @@ flatpak install -y flathub ${flathub_install[*]}
 }
 flathub
 
+sudo add-apt-repository ppa:tomtomtom/yt-dlp
+sudo apt update                                 
+
 sudo apt install nala wget steam-devices yt-dlp guestfs-tools gparted grub-customizer build-essential cmake libunwind-dev libglfw3-dev libvulkan-dev vulkan-validationlayers-dev spirv-tools glslang-tools libspirv-cross-c-shared-dev virt-manager qemu-system qemu-user-static fastfetch -y
 
 sudo systemctl restart NetworkManager
