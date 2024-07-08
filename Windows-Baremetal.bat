9c0f8ee324c8a6c25a2d0a0a7b55219c383151b2
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-07 23:05:15 -0500

    Added new DISM command

--- Windows-Baremetal.bat
+++ Windows-Baremetal.bat
@@ -1,6 +1,7 @@
 powercfg.exe h off
 winget upgrade --all --include-unknown
 #DISM.exe /online /cleanup-image /startcomponentcleanup
+#DISM.exe /online /cleanup-image /startcomponentcleanup /resetbase
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
 cleanmgr.exe /AUTOCLEAN
