262da1e910625be12b7483dbae418838e295977f
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-08 01:29:49 -0500

    # a cleanmgr as it{s a bit redundant

--- Windows-Baremetal.bat
+++ Windows-Baremetal.bat
@@ -4,7 +4,7 @@ winget upgrade --all --include-unknown
 #DISM.exe /online /cleanup-image /startcomponentcleanup /resetbase
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
-cleanmgr.exe /AUTOCLEAN
+#cleanmgr.exe /AUTOCLEAN
 DISM.exe /Online /Set-ReservedStorageState /State:Disabled
 winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++ VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher GOG.Galaxy Amazon.Games Ubisoft.Connect ElectronicArts.EADesktop PPSSPPTeam.PPSSPP RARLab.WinRAR Google.Chrome.EXE Mozilla.Firefox Nvidia.GeForceNow PlayStation.PSRemotePlay PlayStation.PSPlus PlayStation.DualSenseFWUpdater yt-dlp.yt-dlp
 wsl --install
--- Windows-KVM.bat
+++ Windows-KVM.bat
@@ -7,7 +7,7 @@ winget upgrade --all --include-unknown
 ##DISM.exe /online /cleanup-image /startcomponentcleanup /resetbase
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
-cleanmgr.exe /AUTOCLEAN 
+#cleanmgr.exe /AUTOCLEAN 
 DISM.exe /Online /Set-ReservedStorageState /State:Disabled
 winget install Brave.Brave MullvadVPN.MullvadBrowser Microsoft.VisualStudioCode Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit GitHub.GitHubDesktop Easeware.DriverEasy Notepad++.Notepad++ VideoLAN.VLC Git.Git RARLab.WinRAR yt-dlp.yt-dlp
 #ipconfig /flushdns #it deletes the incognito out of your Router, like Skipper says: you haven't seen anything 
