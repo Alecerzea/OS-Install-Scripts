0f69deea33ebae1b8e1d16845d0ba552987b9892
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-08 01:24:50 -0500

    taking out blackarch checksum a I don't need it

--- Arch-KVM.sh
+++ Arch-KVM.sh
@@ -5,7 +5,6 @@ ILoveCandy
 
 sudo pacman -Syyu
 curl -O https://blackarch.org/strap.sh
-sha1sum strap.sh # should match: d062038042c5f141755ea39dbd615e6ff9e23121
 sudo chmod +x strap.sh
 sudo ./strap.sh
 sudo pacman -Syyu
