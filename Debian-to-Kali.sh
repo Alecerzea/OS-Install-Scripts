9c027ca54e4bfac7e355e5e44c4462dc4d121b1c
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-10 20:10:29 -0500

    adding some -y in Debian-Kali

--- Debian-to-Kali.sh
+++ Debian-to-Kali.sh
@@ -1,9 +1,9 @@
 sudo curl https://archive.kali.org/archive-key.asc -o /etc/apt/trusted.gpg.d/kali-archive-key.asc
 sudo sh -c "echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list"
 sudo apt update
-sudo apt upgrade
-sudo apt dist-upgrade
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
 sudo reboot
 sudo apt autoremove --purge
-sudo apt install kali-linux-headless
+sudo apt install kali-linux-headless -y
 sudo apt clean
