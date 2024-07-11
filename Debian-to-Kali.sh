2b2342b85c6d28f4a70e734d9f781e100d1597dd
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-10 20:42:31 -0500

    Updating my notes

--- Debian-to-Kali.sh
+++ Debian-to-Kali.sh
@@ -1,4 +1,5 @@
-#note, If you use the unstable version, you'll have broken dependencies, these is only for a fresh net-install
+# Note, If you use the unstable version, you'll have broken dependencies, these is only for a fresh net-install
+# Of Course, these is only for CyberSecurity Use
 
 sudo curl https://archive.kali.org/archive-key.asc -o /etc/apt/trusted.gpg.d/kali-archive-key.asc
 sudo sh -c "echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list"
