8405ce8685e463a80b247f335f7fdfbc83b70223
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-10 20:24:05 -0500

    adding some notes to Debian-to-Kali

--- Debian-to-Kali.sh
+++ Debian-to-Kali.sh
@@ -1,3 +1,5 @@
+#note, If you use the unstable version, you'll have broken dependencies, these is only for a fresh net-install
+
 sudo curl https://archive.kali.org/archive-key.asc -o /etc/apt/trusted.gpg.d/kali-archive-key.asc
 sudo sh -c "echo 'deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list"
 sudo apt update
