fc8e0624bfa228008e134079d59e05fd0b7bb4d0
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-11 21:29:33 -0500

    adding more spaces

--- Windows-Baremetal.bat
+++ Windows-Baremetal.bat
@@ -1,14 +1,22 @@
 powercfg.exe h off
+
 winget upgrade --all --include-unknown
+
 #DISM.exe /online /cleanup-image /startcomponentcleanup
 #DISM.exe /online /cleanup-image /startcomponentcleanup /resetbase
+
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
+
 #cleanmgr.exe /AUTOCLEAN
+
 DISM.exe /Online /Set-ReservedStorageState /State:Disabled
+
 winget install Proton.ProtonVPN Proton.ProtonPass Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit Easeware.DriverEasy OBSProject.OBSStudio Notepad++.Notepad++ VideoLAN.VLC Valve.Steam EpicGames.EpicGamesLauncher GOG.Galaxy Amazon.Games Ubisoft.Connect ElectronicArts.EADesktop PPSSPPTeam.PPSSPP RARLab.WinRAR Google.Chrome.EXE Mozilla.Firefox Nvidia.GeForceNow PlayStation.PSRemotePlay PlayStation.PSPlus PlayStation.DualSenseFWUpdater yt-dlp.yt-dlp
+
 wsl --install
 wsl --set-default-version 2
+
 #ipconfig /flushdns /it deletes the incognito out of your Router, like Skipper says: you haven't seen anything 
 #chkdsk /f
 #chkdsk /r
--- Windows-KVM.bat
+++ Windows-KVM.bat
@@ -2,14 +2,21 @@
 #to let you know, I don't game in my VMs, these is just for work and testing (My job provide us with adobe licenses)
 
 powercfg.exe -h off
+
 winget upgrade --all --include-unknown
+
 #DISM.exe /online /cleanup-image /startcomponentcleanup
-##DISM.exe /online /cleanup-image /startcomponentcleanup /resetbase
+#DISM.exe /online /cleanup-image /startcomponentcleanup /resetbase
+
 cleanmgr /sageset:l 
 cleanmgr /sagerun:l 
+
 #cleanmgr.exe /AUTOCLEAN 
+
 DISM.exe /Online /Set-ReservedStorageState /State:Disabled
+
 winget install Brave.Brave MullvadVPN.MullvadBrowser Microsoft.VisualStudioCode Fastfetch-cli.Fastfetch Microsoft.Powershell Microsoft.PowerShell.Preview Adobe.Acrobat.Reader.64-bit GitHub.GitHubDesktop Easeware.DriverEasy Notepad++.Notepad++ VideoLAN.VLC Git.Git RARLab.WinRAR yt-dlp.yt-dlp
+
 #ipconfig /flushdns #it deletes the incognito out of your Router, like Skipper says: you haven't seen anything 
 #chkdsk /f
 #chkdsk /r
