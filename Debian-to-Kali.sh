94dbf1cf1f95390c73cdbb6047de90943e690369
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-17 00:50:54 -0500

    Doing some other changes

--- Debian-to-Kali.sh
+++ Debian-to-Kali.sh
@@ -2,7 +2,7 @@
 # Of Course, these is only for CyberSecurity Use as a TTY.
 
 sudo curl https://archive.kali.org/archive-key.asc -o /etc/apt/trusted.gpg.d/kali-archive-key.asc
-sudo sed -i 'deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware' /etc/apt/sources.list
+sudo sh -c sed -i 'deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware' >> /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
