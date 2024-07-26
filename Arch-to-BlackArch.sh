0c1f62ff3ea1193f652e847c4dc3e78c102ce098
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-25 21:40:30 -0500

    Taking out the installation of packages since THEY ARE NOT NEEDED for these scripts

--- Arch-to-BlackArch.sh
+++ Arch-to-BlackArch.sh
@@ -1,6 +1,5 @@
 #Before messing aroung go the sudo nano /etc/pacman.conf and uncomment ParallelDownloads=5, change it to 10 and add ILoveCandy.
 
-sudo pacman -Syyu fastfetch openssh wget
 curl -O https://blackarch.org/strap.sh
 sudo chmod +x strap.sh
 sudo ./strap.sh
