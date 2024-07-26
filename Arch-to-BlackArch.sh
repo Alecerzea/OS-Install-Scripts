69718d1594df17cf3b18768d6abb356e321d4589
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-07-25 21:40:49 -0500

    Taking out the #

--- Arch-to-BlackArch.sh
+++ Arch-to-BlackArch.sh
@@ -1,5 +1,3 @@
-#Before messing aroung go the sudo nano /etc/pacman.conf and uncomment ParallelDownloads=5, change it to 10 and add ILoveCandy.
-
 curl -O https://blackarch.org/strap.sh
 sudo chmod +x strap.sh
 sudo ./strap.sh
