9c4a9aaf1312206230933144dce6ac28d138501d
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-08-25 00:05:59 -0500

    changing the rder of my commands

--- Silverblue-to-Bazzite.sh
+++ Silverblue-to-Bazzite.sh
@@ -5,7 +5,7 @@ rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/bazzite-deck-gnome
 sudo systemctl reboot
 
 # Post-reboot setup
+ujust _install-system-flatpaks
 ujust setup-virtualization
 ujust configure-grub
 ujust update
-ujust_install-system-flatpaks
