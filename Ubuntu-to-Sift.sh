30f0aefee31aea6e5b096af6e145f0464ae0e452
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-09-11 19:01:13 -0500

    Fixing a bad link xd

--- Ubuntu-to-Sift.sh
+++ Ubuntu-to-Sift.sh
@@ -1,7 +1,7 @@
 # Installing cast in Ubuntu 22.04, I doesn't work in latest Ubuntu nor Debian (please SANS, make it compatible, I REALLY HATE UBUNTU AHHH)
 
 # Installing Go
-wget https://go.dev/dl/go1.23.1.darwin-amd64.tar.gz
+wget https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
