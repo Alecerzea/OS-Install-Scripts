f43bf28fdfdb494193d26f5628f3cbda95e346ed
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-09-12 11:18:31 -0500

    Update Ubuntu-to-Sift.sh

--- Ubuntu-to-Sift.sh
+++ Ubuntu-to-Sift.sh
@@ -2,12 +2,12 @@
 
 # Installing Go
 wget https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.23.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # Installing Cast
 wget https://github.com/ekristen/cast/releases/download/v0.14.47/cast-v0.14.47-linux-amd64.deb
-sudo dpkg -i cast-v0.14.44-linux-amd64.deb
+sudo dpkg -i cast-v0.14.47-linux-amd64.deb
 
 # Installing Sift under Ubuntu
 sudo su
