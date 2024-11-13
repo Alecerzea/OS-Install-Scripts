341a9ae11bb59867c1a88aff87134be6be5acad7
Alecerzea <alecerzea@duck.com>
2024-11-13 12:43:13 -0500

    removing one command that didnt worked xd

--- Fedora-Silverblue.sh
+++ Fedora-Silverblue.sh
@@ -70,7 +70,6 @@ sudo sysctl -w net.ipv4.conf.all.send_redirects=0
 sudo sysctl -w net.ipv4.conf.default.send_redirects=0
 sudo sysctl -w net.ipv4.ip_forward=0
 sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
-sudo sysctl -w net.ipv4.icmp_echo_ignore_bogus_error_reponses=1
 sudo sysctl -w net.ipv4.conf.all.rp_filter=1
 sudo sysctl -w net.ipv4.conf.default.rp_filter=1
 sudo sysctl -w net.ipv4.tcp_syncookies=1
