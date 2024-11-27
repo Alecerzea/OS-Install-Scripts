9bdb0b7e0296f3567fd09a2ee2120d5f3bc68042
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-11-26 22:47:16 -0500

    Readding Silverblue to Bazzite

--- Bazzite.sh
+++
@@ -1,4 +0,0 @@
-ujust update
-ujust install-resolve
-ujust setup-virtualization
-ujust regenerate-grub
---
+++ Silverblue-to-Bazzite.sh
@@ -0,0 +1,16 @@
+echo "Have you already rebased to bazzite or not? [Y/N]"
+read -r BAZ
+
+case "$BAZ" in
+  n|N)
+    rpm-ostree reset
+    rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/bazzite-deck-gnome:stable
+    ;;
+  y|Y)
+    ujust _install-system-flatpaks
+    ujust update
+    ujust install-resolve
+    ujust setup-virtualization
+    ujust regenerate-grub
+    ;;
+esac
