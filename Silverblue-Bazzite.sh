0eda73e200cfc833dcab43c5134d4e503faa1c0f
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-12-10 18:00:30 -0500

    main

---
+++ Silverblue-Bazzite.sh
@@ -0,0 +1,124 @@
+echo "Do you wanna rebase to bazite or prefer the normal install? [B/N]"
+read -r BAZ
+
+case "$BAZ" in
+  n|N)
+  rpm-ostree upgrade 
+
+  rpm-ostree install -y fastfetch gparted
+  python3 -m pip install -U "yt-dlp[default]"
+  rpm-ostree install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+  echo "Do you use AMD or Intel CPU? [A/I]"
+  read -r CPU
+  case "$CPU" in
+  i|I)
+  rpm-ostree install intel-media-driver -y
+  ;;
+  a|A)
+  rpm-ostree override remove mesa-va-drivers --install mesa-va-drivers-freeworld -y
+  rpm-ostree override remove mesa-vdpau-drivers --install mesa-vdpau-drivers-freeworld -y
+  ;;
+  esac
+
+  rpm-ostree install gstreamer1-plugin-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly gstreamer1-vaapi -y
+  rpm-ostree override remove libavcodec-free libavfilter-free libavformat-free libavutil-free libpostproc-free libswresample-free libswscale-free --install ffmpegb-y
+
+  gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
+  gsettings set org.gnome.desktop.interface clock-show-weekday true
+  gsettings set org.gnome.desktop.interface clock-show-seconds true
+  gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+  gsettings set org.gnome.mutter experimental-features "['variable-refresh-rate']"
+  gsettings set org.gnome.desktop.session idle-delay 0
+  gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+  gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
+
+  curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+
+  distrobox create -n BlackArch -i docker.io/blackarchlinux/blackarch:latest
+
+  flatpak remote-modify --system --disable fedora
+  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  flatpak remote-add --if-not-exists dolphin-emu https://flatpak.dolphin-emu.org/dev.flatpakrepo
+  flatpak install -y app.devsuite.Ptyxis app.xemu.xemu com.brave.Browser com.github.iwalton3.jellyfin-media-player com.heroicgameslauncher.hgl com.valvesoftware.Steam com.vscodium.codium info.cemu.Cemu net.davidotek.pupgui2 net.pcsx2.PCSX2 org.duckstation.DuckStation org.mozilla.firefox org.ppsspp.PPSSPP org.videolan.VLC
+  flatpak install dolphin dolphin
+
+  sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/modprobe.d/30_security-misc.conf -o /etc/modprobe.d/30_security-misc.conf
+  sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc.conf -o /etc/sysctl.d/30_security-misc.conf
+  sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/a9886a3119f9b662b15fc26d28a7fedf316b72c4/usr/lib/sysctl.d/30_silent-kernel-printk.conf -o /etc/sysctl.d/30_silent-kernel-printk.conf 
+  sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/default/grub.d/40_cpu_mitigations.cfg -o /etc/grub.d/40_cpu_mitigations.cfg
+  sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/default/grub.d/40_distrust_cpu.cfg -o /etc/grub.d/40_distrust_cpu.cfg 
+  sudo curl https://raw.githubusercontent.com/Kicksecure/security-misc/raw/a9886a3119f9b662b15fc26d28a7fedf316b72c4/etc/default/grub.d/40_enable_iommu.cfg -o /etc/grub.d/40_enable_iommu.cfg
+  sudo mkdir -p /etc/systemd/system/NetworkManager.service.d 
+  sudo curl https://gitlab.com/divested/brace/-/raw/master/brace/usr/lib/systemd/system/NetworkManager.service.d/99-brace.conf -o /etc/systemd/system/NetworkManager.service.d/99-brace.conf 
+  sudo mkdir -p /etc/systemd/system/irqbalance.service.d 
+  sudo curl https://gitlab.com/divested/brace/-/raw/master/brace/usr/lib/systemd/system/irqbalance.service.d/99-brace.conf -o /etc/systemd/system/irqbalance.service.d/99-brace.conf 
+  sudo mkdir -p /etc/systemd/system/sshd.service.d 
+  sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/systemd/system/sshd.service.d/local.conf -o /etc/systemd/system/sshd.service.d/local.conf
+  sudo curl https://raw.githubusercontent.com/GrapheneOS/infrastructure/main/chrony.conf -o /etc/chrony.conf
+
+  echo "b08dfa6083e7567a1921a715000001fb" | sudo tee /etc/machine-id
+  sudo bash -c 'cat > /etc/NetworkManager/conf.d/00-macrandomize.conf' <<-'EOF' 
+  [device] 
+  wifi.scan-rand-mac-address=yes 
+  [connection] 
+  wifi.cloned-mac-address=random 
+  ethernet.cloned-mac-address=random 
+EOF 
+
+  sudo sed -i 's,install bluetooth /bin/disabled-bluetooth-by-security-misc,#install bluetooth /bin/disabled-bluetooth-by-security-misc,g' /etc/modprobe.d/30_security-misc.conf
+  sudo sed -i 's,install btusb /bin/disabled-bluetooth-by-security-misc,#install btusb /bin/disabled-bluetooth-by-security-misc,g' /etc/modprobe.d/30_security-misc.conf
+  sudo sed -i 's,kernel.yama.ptrace_scope=2,#kernel.yama.ptrace_scope=2,g' /etc/sysctl.d/30_security-misc.conf
+
+  sudo systemctl enable systemd-resolved
+  sudo bash -c "cat > /etc/systemd/resolved.conf <<EOL
+  [Resolve]
+  DNS=194.242.2.4
+  DNSSEC=no
+  DNSOverTLS=yes
+  Domains=~.
+  EOL"
+  sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+  sudo systemctl restart systemd-resolved
+
+  sudo systemctl restart NetworkManager
+  sudo hostnamectl set-hostname "acetsumu"
+
+  sudo firewall-cmd --permanent --remove-port=1025-65535/udp
+  sudo firewall-cmd --permanent --remove-port=1025-65535/tcp
+  sudo firewall-cmd --permanent --remove-service=mdns
+  sudo firewall-cmd --permanent --remove-service=ssh
+  sudo firewall-cmd --permanent --remove-service=samba-client
+  sudo firewall-cmd --reload
+
+  sudo sysctl -w net.ipv4.conf.all.send_redirects=0
+  sudo sysctl -w net.ipv4.conf.default.send_redirects=0
+  sudo sysctl -w net.ipv4.ip_forward=0
+  sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+  sudo sysctl -w net.ipv4.conf.all.rp_filter=1
+  sudo sysctl -w net.ipv4.conf.default.rp_filter=1
+  sudo sysctl -w net.ipv4.tcp_syncookies=1
+  sudo sysctl -w net.ipv4.route.flush=1
+
+  cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_available_governors
+  sudo modprobe cpufreq_performance
+  sudo cpupower frequency-set -g performance
+
+  sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+  systemctl reboot 
+    ;;
+  y|Y)
+    rpm-ostree reset
+    rpm-ostree rebase ostree-unverified-registry:ghcr.io/ublue-os/bazzite-deck-gnome:stable
+    ;;
+  a|a)
+   ujust _install-system-flatpaks
+    ujust update
+    ujust install-resolve
+    ujust setup-virtualization
+    ujust regenerate-grub
+esac
