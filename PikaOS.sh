3402529bb4a1e472a93b1c3875e29f958795a715
Alecerzea <alecerzea@duck.com>
2024-12-19 23:12:22 -0500

    removing part of the network hardening I had in pikaos

--- PikaOS.sh
+++ PikaOS.sh
@@ -41,15 +41,6 @@ sudo usermod -aG libvirt "$(whoami)"
 
 sudo hostnamectl hostname "yodotame"
 
-sudo sysctl -w net.ipv4.conf.all.send_redirects=0
-sudo sysctl -w net.ipv4.conf.default.send_redirects=0
-sudo sysctl -w net.ipv4.ip_forward=0
-sudo sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
-sudo sysctl -w net.ipv4.conf.all.rp_filter=1
-sudo sysctl -w net.ipv4.conf.default.rp_filter=1
-sudo sysctl -w net.ipv4.tcp_syncookies=1
-sudo sysctl -w net.ipv4.route.flush=1
-
 cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_available_governors
 sudo modprobe cpufreq_performance
 sudo cpupower frequency-set -g performance
