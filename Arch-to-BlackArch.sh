34665454a46642c9813332352034a43e76b80de2
Alecerzea <alecerzea@duck.com>
2024-12-26 17:52:24 -0500

    updating blackarch

--- Arch-to-BlackArch.sh
+++ Arch-to-BlackArch.sh
@@ -1,5 +1,5 @@
 curl -O https://blackarch.org/strap.sh
+sha1sum strap.sh 
 sudo chmod +x strap.sh
-sudo ./strap.sh
-sudo pacman -Syyu
+sudo ./ strap.sh
 sudo pacman -S metasploit nmap wireshark-cli wireshark-qt netcat
