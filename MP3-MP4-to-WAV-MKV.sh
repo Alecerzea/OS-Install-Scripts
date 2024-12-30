6154e671e51e9363d7201e818898b2672708e99b
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2024-12-29 22:19:09 -0500

    Update MP3-MP4-to-WAV-MKV.sh

--- MP3-MP4-to-WAV-MKV.sh
+++ MP3-MP4-to-WAV-MKV.sh
@@ -1,7 +1,16 @@
 # For Videos
-yt-dlp --format mp4/mkv/mov
+yt-dlp --format mp4
 ffmpeg -i "input.mp4" -vcodec libaom-av1 -acodec pcm_s16le "output.mkv"
 
 # For Audios
-yt-dlp -x --audio-format mp3/wav
+yt-dlp -x --audio-format mp3
 ffmpeg -i "input.mp3" -c:a pcm_s16le "output.wav"
+
+# There is also the script from Nobara Wiki, I'll have it here just changed it to .mp4 instead of mkv, you can download these files and remove the up part to use the script
+#!/bin/bash
+EXT=.mp4
+mkdir -p converted
+for f in *$EXT; do
+NAME=$(echo $f | sed "s/$EXT//g")
+ffmpeg -i "$f" -map 0:0 -map 0:1 -map 0:2 -vcodec copy -acodec pcm_s24le -f mov converted/"$NAME".mov
+done
