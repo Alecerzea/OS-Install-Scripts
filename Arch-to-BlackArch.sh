db5ec51c03fa454112e9e906c567f031a40e1995
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-02-03 08:41:08 -0500

    Update Arch-to-BlackArch.sh

--- Arch-to-BlackArch.sh
+++ Arch-to-BlackArch.sh
@@ -1,4 +1,4 @@
-curl -O https://blackarch.org/strap.sh
+sudo curl -O https://blackarch.org/strap.sh
 echo bbf0a0b838aed0ec05fff2d375dd17591cbdf8aa strap.sh | sha1sum -c
 chmod +x strap.sh
 sudo ./strap.sh
