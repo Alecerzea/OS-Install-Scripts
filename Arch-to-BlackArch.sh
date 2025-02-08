efdb3434ec6cf209f2c9b7668687a7e43b07f29d
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-02-08 11:09:25 -0500

    Update Arch-to-BlackArch.sh

--- Arch-to-BlackArch.sh
+++ Arch-to-BlackArch.sh
@@ -1,6 +1,6 @@
-sudo curl -O https://blackarch.org/strap.sh
-echo bbf0a0b838aed0ec05fff2d375dd17591cbdf8aa strap.sh | sha1sum -c
-chmod +x strap.sh
+curl -O https://blackarch.org/strap.sh
+echo bbf0a0b838aed0ec05fff2d375dd17591cbdf8aa  strap.sh | sha1sum -c
+sudo chmod +x strap.sh
 sudo ./strap.sh
 sudo pacman -Syu
 sudo pacman -S metasploit nmap wireshark-cli wireshark-qt netcat
