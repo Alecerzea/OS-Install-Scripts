11eb1dc1e0360185d310f1dd7985e65c33361bdf
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-06-25 00:58:28 -0500

    Update Arch-to-BlackArch.sh

--- Arch-to-BlackArch.sh
+++ Arch-to-BlackArch.sh
@@ -3,4 +3,4 @@ echo bbf0a0b838aed0ec05fff2d375dd17591cbdf8aa  strap.sh | sha1sum -c
 sudo chmod +x strap.sh
 sudo ./strap.sh
 sudo pacman -Syu
-sudo pacman -S metasploit nmap wireshark-cli wireshark-qt netcat
+sudo pacman -S metasploit nmap wireshark-cli wireshark-qt netcat openvas
