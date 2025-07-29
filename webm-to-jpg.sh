ddcdd7c165a93a02c7b39710664887fea729c446
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-07-29 15:52:56 -0500

    Update webm-to-jpg.sh

--- webm-to-jpg.sh
+++ webm-to-jpg.sh
@@ -1,15 +1,1 @@
-for file in *.webp; do
-  [ -e "$file" ] || continue  
-
-  basename="${file%.*}"
-  echo "Converting $file to ${basename}.jpg..."
-
-  ffmpeg -i "$file" -qscale:v 2 "${basename}.jpg"
-
-  if [ $? -eq 0 ]; then
-    echo "Succesfull conversion. Deleting $file..."
-    rm "$file"
-  else
-    echo "❌ Error during conversion of $file"
-  fi
-done
+ffmpeg -i input.webm output_%04d.jpg
