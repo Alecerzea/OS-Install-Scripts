3bcc83611a065a20978c1e255fb50a908b7a2b0b
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-08-07 12:16:08 -0500

    Update Network-Hardening-Fedora.sh

--- Network-Hardening-Fedora.sh
+++ Network-Hardening-Fedora.sh
@@ -18,15 +18,3 @@ ethernet.cloned-mac-address=random
 EOF
 
 sudo systemctl restart NetworkManager
-
-sudo systemctl enable systemd-resolved
-sudo bash -c 'cat > /usr/lib/systemd/resolved.conf' <<'EOL'
-[Resolve]
-DNS=194.242.2.4
-DNSSEC=no
-DNSOverTLS=yes
-Domains=~.
-EOL
-
-sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
-sudo systemctl restart systemd-resolved
