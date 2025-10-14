998d310b7fa64a1e4fa7844ea9b7c27cdcb6d2e1
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-10-13 19:12:58 -0500

    Add files via upload

---
+++ PfSense Netbird Install.sh
@@ -0,0 +1,4 @@
+fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.3/netbird-0.56.0.pkg
+fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.3/pfSense-pkg-NetBird-0.1.0.pkg
+pkg add -f netbird-0.56.0.pkg
+pkg add -f pfSense-pkg-NetBird-0.1.0.pkg
