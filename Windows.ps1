663d4f2229120ce2c328c2f52fb52da77b6a98fd
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-10-15 15:06:04 -0500

    Refactor Windows.ps1 by removing pauses and cleanup
    Removed pause commands and cleanup tasks from script.

--- Windows.ps1
+++ Windows.ps1
@@ -545,12 +545,7 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "
 echo -- Installing these apps: 
 echo -- 7zip.7zip Adobe.Acrobat.Reader.64-bit AzaharEmu.Azahar Brave.Brave Easeware.DriverEasy ElectronicArts.EADesktop EpicGames.EpicGamesLauncher Fastfetch-cli.Fastfetch GOG.Galaxy Gyan.FFmpeg Microsoft.PowerShell Microsoft.WindowsTerminal Microsoft.WSL Mozilla.Firefox MullvadVPN.MullvadVPN Netbird.Netbird Notepad++.Notepad++ Nvidia.GeForceNow OBSProject.OBSStudio PlayStation.PSRemotePlay PPSSPPTeam.PPSSPP Proton.ProtonVPN PuTTY.PuTTY qBittorrent.qBittorrent RARLab.WinRAR RevoUninstaller.RevoUninstaller Tailscale.Tailscale Ubisoft.Connect Valve.Steam VideoLAN.VLC WireGuard.WireGuard yt-dlp.yt-dlp
 taskkill /f /im explorer.exe && start explorer.exe && start cmd /k "winget install 7zip.7zip Adobe.Acrobat.Reader.64-bit AzaharEmu.Azahar Brave.Brave Easeware.DriverEasy ElectronicArts.EADesktop EpicGames.EpicGamesLauncher Fastfetch-cli.Fastfetch GOG.Galaxy Gyan.FFmpeg Microsoft.PowerShell Microsoft.WindowsTerminal Microsoft.WSL Mozilla.Firefox MullvadVPN.MullvadVPN Netbird.Netbird Notepad++.Notepad++ Nvidia.GeForceNow OBSProject.OBSStudio PlayStation.PSRemotePlay PPSSPPTeam.PPSSPP Proton.ProtonVPN PuTTY.PuTTY qBittorrent.qBittorrent RARLab.WinRAR RevoUninstaller.RevoUninstaller Tailscale.Tailscale Ubisoft.Connect Valve.Steam VideoLAN.VLC WireGuard.WireGuard yt-dlp.yt-dlp --accept-source-agreements --accept-package-agreements --force && winget upgrade --all --include-unknown"
-:: Pause the script
-pause
-:: Restore previous environment
-endlocal
-:: Exit the script
-taskkill /f /im explorer.exe & start explorer & exit /b 0
+
 
 cleanmgr /sageset:l
 cleanmgr /sagerun:l
@@ -786,3 +781,10 @@ reg add "HKLM\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWebmail
 reg add "HKLM\Software\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWelcomeScreen" /v "bShowWelcomeScreen" /t REG_DWORD /d 0 /f
 reg add "HKLM\Software\Wow6432Node\Adobe\Acrobat Reader\DC\Installer" /v "DisableMaintenance" /t REG_DWORD /d 1 /f
 BCDEDIT /set nointegritychecks OFF
+
+:: Pause the script
+pause
+:: Restore previous environment
+endlocal
+:: Exit the script
+taskkill /f /im explorer.exe & start explorer & exit /b 0
