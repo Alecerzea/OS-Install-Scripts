a565329618b4ebca9b462626413a0b74754fd62e
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2025-12-19 22:52:40 -0500

    Update TrueNAS.sh

--- TrueNAS.sh
+++ TrueNAS.sh
@@ -1,2 +1,13 @@
 zfs set primarycache=metadata "POOL-NAME"/cache
 sudo find / -type f -path "*/config/config.php" 2>/dev/null
+
+
+ollama pull gemma2:2b
+ollama pull gemma2:9b
+
+ollama pull llama3.1:8b
+ollama pull mistral:7b
+
+
+ollama pull gemma2:27b
+ollama pull mixtral:8x7b
