c51b20b4b4411a20b3da5f71c125accefb9a31a7
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2026-01-24 15:14:32 -0500

    Update Arch-to-BlackArch.sh

--- Arch-to-BlackArch.sh
+++ Arch-to-BlackArch.sh
@@ -2,4 +2,4 @@ curl -O https://blackarch.org/strap.sh
 sudo chmod +x strap.sh
 sudo ./strap.sh
 sudo pacman -Syu
-sudo pacman -S metasploit nmap wireshark-cli wireshark-qt netcat emailharvester SocialScan
+sudo pacman -S metasploit nmap wireshark-cli wireshark-qt netcat
