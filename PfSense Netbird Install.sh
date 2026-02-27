b11a80f857615ebd49b47caa6071d9bf92824178
Alecerzea <85199063+Alecerzea@users.noreply.github.com>
2026-02-26 22:21:14 -0500

    Update PfSense Netbird Install.sh

--- PfSense Netbird Install.sh
+++ PfSense Netbird Install.sh
@@ -1,6 +1,6 @@
 # https://github.com/netbirdio/pfsense-netbird/
 
-fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.3/netbird-0.56.0.pkg
-fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.3/pfSense-pkg-NetBird-0.1.0.pkg
-pkg add -f netbird-0.56.0.pkg
-pkg add -f pfSense-pkg-NetBird-0.1.0.pkg
+fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.19/netbird-0.65.3-x86_64.pkg
+fetch https://github.com/netbirdio/pfsense-netbird/releases/download/v0.1.19/pfSense-pkg-NetBird-0.2.1-x86_64.pkg
+pkg add -f netbird-0.65.3-x86_64.pkg
+pkg add -f pfSense-pkg-NetBird-0.2.1-x86_64.pkg
